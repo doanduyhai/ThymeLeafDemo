77e45fadfcd5d8f52eb74151f5db1fac9a77274a
Tanya Pascal <josephinex21@gmail.com>
2017-10-16 16:58:22 +0300

    add styles

--- src/main/java/doan/thymeleaf/demo/domain/Employee.java
+++ src/main/java/doan/thymeleaf/demo/domain/Employee.java
@@ -5,10 +5,28 @@ public class Employee {
 	private String firstName;
 
 	private String lastName;
+	
+	private int age;
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 
 	public Employee(String firstName, String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
+	}
+	
+	public Employee(String firstName, String lastName, int age) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
 	}
 
 
